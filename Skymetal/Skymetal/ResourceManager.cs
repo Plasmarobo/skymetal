618643e9924c1f9c73cf8796b8c60ee3191ce1b8
Austen Higgins-Cassidy <plasmarobo@gmail.com>
2015-01-06 19:09:30 -0500

    Moar files

---
+++ Skymetal/Skymetal/ResourceManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Skymetal
+{
+    class ResourceManager
+    {
+        protected Dictionary<String, Texture2D> mTextures;
+        protected Dictionary<String, SoundEffect> mSoundEffects;
+    }
+}
